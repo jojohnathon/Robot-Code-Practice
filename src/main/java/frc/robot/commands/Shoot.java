994598072632da5a60694f032b5e577316957866
Ronit Barman <ronitbarman493@gmail.com>
2022-01-25 19:15:41 -0800

    add basic shooter functionality

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -5,12 +5,35 @@ import java.util.Set;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.RobotContainer;
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Shoot implements Command {
     private Subsystem[] requirements = {RobotContainer.shooter};
-    public Shoot() {
+    private double speed;
+    Timer shootTimer;
 
+    public Shoot(double speed) {
+        this.speed = speed;
+        shootTimer = new Timer();
     }
+
+    @Override
+    public void initialize() {
+        shootTimer.start();
+        shootTimer.reset();
+    }
+
+    @Override
+    public void execute() {
+        Shooter.getInstance().setOpenLoop(this.speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Shooter.getInstance().stop();
+    }
+    
     public Set<Subsystem> getRequirements() {
         return Set.of(requirements);
     }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -10,8 +12,10 @@ import frc.robot.Constants.ShooterConstants;
 public class Shooter implements Subsystem {
     private static final CANSparkMax master = Util.createSparkMAX(ShooterConstants.master, MotorType.kBrushless);
     private static final CANSparkMax slave = Util.createSparkMAX(ShooterConstants.slave, MotorType.kBrushless);
+    private static RelativeEncoder encoder = master.getEncoder();
 
     private static Shooter instance = null;
+
     private Shooter() {
         slave.follow(master);
     }
@@ -19,5 +23,17 @@ public class Shooter implements Subsystem {
     public static Shooter getInstance() {
         if(instance == null) instance = new Shooter();
         return instance;
+    }
+
+    public void setOpenLoop(final double value){
+        master.set(value);
+    }
+
+    public void stop(){
+        master.stopMotor();
+    }
+
+    public double getShooterVelocity() {
+        return encoder.getVelocity();
     }
 }
