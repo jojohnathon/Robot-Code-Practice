3ecb16b3e1b6cdb20bb88b870c8672c49ba42043
Beavis <pigmaster50@gmail.com>
2022-01-27 09:45:41 -0800

    Update DriveXMeters.java

--- src/main/java/frc/robot/commands/DriveXMeters.java
+++ src/main/java/frc/robot/commands/DriveXMeters.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
+import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.Drivetrain;
 
 public class DriveXMeters implements Command {
@@ -13,12 +14,22 @@ public class DriveXMeters implements Command {
     private Subsystem[] requirements = {drivetrain};
     private TrapezoidProfile.State goal;
     private TrapezoidProfile profile;
+    private TrapezoidProfile.Constraints constraints;
     
+    /*
+    @param goal     the goal in terms of distance & max speed: the actual goal needs to be modified for speed 0, the endpoint
+    */
     public DriveXMeters(TrapezoidProfile.State goal) {
-        this.goal = goal;
-        profile = new TrapezoidProfile(Drivetrain.constraints, this.goal);
+        this.goal.position = goal.position;
+        this.goal.velocity = 0;
+        constraints = new TrapezoidProfile.Constraints(goal.velocity, DrivetrainConstants.kMaxAcceleration);
+        profile = new TrapezoidProfile(constraints, this.goal);
     }
 
+    /*
+        @param distance     desired distance
+        @param maxSpeedMPS  max speed during motion
+    */
     public DriveXMeters(double distance, double maxSpeedMPS) {
         this(new TrapezoidProfile.State(distance, maxSpeedMPS));
 
