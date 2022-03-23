c67778561e00d36c07ab6dbede50b4375fe951a4
bobabot253 <253programming@gmail.com>
2022-03-23 14:49:10 -0700

    Print navx reading

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -60,11 +60,7 @@ public class Drivetrain implements Subsystem {
         SmartDashboard.putNumber("Left Master output: ", leftMaster.getMotorOutputPercent());
         //SmartDashboard.putNumber("Left Slave output: ", leftSlave.getMotorOutputPercent());
         SmartDashboard.putNumber("Right Master output: ", rightMaster.getMotorOutputPercent());
-        SmartDashboard.putNumber("falcon500 stator", leftSlave.getStatorCurrent());
-        SmartDashboard.putNumber("falcon500 bus", leftSlave.getBusVoltage());
-        SmartDashboard.putNumber("falcon500 supply", leftSlave.getSupplyCurrent());
-        SmartDashboard.putNumber("falcon500 temp", leftSlave.getTemperature());
-        SmartDashboard.putNumber("kInverted", kInverted);
+        SmartDashboard.putNumber("NavX heading", RobotContainer.navX.getAngle());
         //SmartDashboard.putNumber("Right Slave output: ", rightSlave.getMotorOutputPercent());
         
     }
